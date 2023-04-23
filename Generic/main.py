84ce30934c0b526da6dc269094f80750e1d2c90e
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-04-23 08:43:28 +0200

    Update main.py

--- Generic/main.py
+++ Generic/main.py
@@ -8,6 +8,6 @@ sock.listen(1)
 while True:
   client, address = sock.accept()
   try:
-    client.send(f"HTTP/1.1 200 OK\n\n{open(f'{input('Server Dir: ')}{client.recv(1024).decode().splitlines()[0].split()[1]}', 'r').read()}".encode('utf-8'))
+    client.send(f"HTTP/1.1 200 OK\n\n{open(f'{client.recv(1024).decode().splitlines()[0].split()[1]}', 'r').read()}".encode('utf-8'))
   except (FileNotFoundError, IsADirectoryError): 
     client.send("HTTP/1.1 404 Not Found".encode("utf-8"))
