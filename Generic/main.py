4fd7f8ef25a64dd3d42299d99aad6b0ec6155a6e
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-04-23 08:52:31 +0200

    Update main.py

--- Generic/main.py
+++ Generic/main.py
@@ -1,7 +1,7 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 sock.bind(("0.0.0.0", 8080))
 sock.listen(1)
 
