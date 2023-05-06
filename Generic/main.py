4bb853e9afa671b7747110196318cbc3a0af251b
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-06 19:45:51 +0200

    Update main.py

--- Generic/main.py
+++ Generic/main.py
@@ -3,10 +3,10 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.bind(("0.0.0.0", 8080))
-sock.listen(1)
+sock.listen(5)
 
 while True:
-  client, address = sock.accept()
+  client, _ = sock.accept()
   try:
     client.send(f"HTTP/1.1 200 OK\n\n{open(f'.{client.recv(1024).decode().splitlines()[0].split()[1]}', 'r').read()}".encode('utf-8'))
   except (FileNotFoundError, IsADirectoryError): 
