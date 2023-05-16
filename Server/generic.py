51473597428143f05af198362925f58d2e244091
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-16 21:34:29 +0200

    Binary Issue

--- Server/generic.py
+++ Server/generic.py
@@ -1,4 +1,5 @@
 import socket as k
+import mimetypes
 
 s = k.socket(k.AF_INET, k.SOCK_STREAM)
 s.bind(("0.0.0.0", 8080))
@@ -7,6 +8,7 @@ s.listen(5)
 while True:
   c, _ = s.accept()
   try:
-    c.send(f"HTTP/1.1 200 OK\n\n{open(f'.{c.recv(1024).decode().split()[1]}', 'r').read()}".encode())
-  except (FileNotFoundError, IsADirectoryError): 
+    path = c.recv(1024).decode().split()[1]
+    c.send(f"HTTP/1.1 200 OK\nContent-Type: {mimetypes.guess_type(f'.{path}')}\n\n".encode() + open(f'.{path}', 'rb').read())
+  except (FileNotFoundError, IsADirectoryError):
     c.send("HTTP/1.1 404 Not Found".encode("utf-8"))
