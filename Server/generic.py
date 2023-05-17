d093d37af57831fe6cb1c317b641146cc5c54609
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-17 20:24:41 +0200

    Update generic.py

--- Server/generic.py
+++ Server/generic.py
@@ -1,6 +1,6 @@
 import socket as k
-import mimetypes
 import sys
+
 
 s = k.socket(k.AF_INET, k.SOCK_STREAM)
 s.bind(("0.0.0.0", int(sys.argv[1])))
@@ -10,5 +10,5 @@ while True:
   c, _ = s.accept()
   try:
     path = c.recv(1024).decode().split()[1]
-    c.send(f"HTTP/1.1 200 OK\nContent-Type: {mimetypes.guess_type(f'.{path}')}\n\n".encode() + open(f'.{path}', 'rb').read())
+    c.send("HTTP/1.1 200 OK\n\n".encode() + open(f'.{path}', 'rb').read())
   except FileNotFoundError, IsADirectoryError: c.send("HTTP/1.1 404 Not Found".encode("utf-8"))
