ffe1332c2466432db0f640b7e098af784915c051
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-17 17:50:27 +0200

    Tweaks

--- Server/generic.py
+++ Server/generic.py
@@ -1,4 +1,5 @@
 import socket as k
+import mimetypes
 import sys
 
 s = k.socket(k.AF_INET, k.SOCK_STREAM)
@@ -9,6 +10,5 @@ while True:
   c, _ = s.accept()
   try:
     path = c.recv(1024).decode().split()[1]
-    if path == "/": path = "/index.html"
-    c.send(f"HTTP/1.1 200 OK\n\n".encode() + open(f'.{path}', 'rb').read())
-  except (FileNotFoundError, IsADirectoryError): c.send("HTTP/1.1 404 Not Found".encode("utf-8"))
+    c.send(f"HTTP/1.1 200 OK\nContent-Type: {mimetypes.guess_type(f'.{path}')}\n\n".encode() + open(f'.{path}', 'rb').read())
+  except FileNotFoundError, IsADirectoryError: c.send("HTTP/1.1 404 Not Found".encode("utf-8"))
