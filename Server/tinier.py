9e61ab9a86d154d37e52db592425fac534e9f4d2
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-17 17:31:33 +0200

    better

--- Server/tinier.py
+++ Server/tinier.py
@@ -3,6 +3,8 @@ import socket as k
 s = k.socket(k.AF_INET, k.SOCK_STREAM)
 s.bind(("0.0.0.0", 8080))
 s.listen(5)
+
 while True:
   c, _ = s.accept()
-    c.send(f"HTTP/1.1 200 OK\n\n{open(f'.{c.recv(1024).decode().split()[1]}', 'r').read()}".encode())
+  try: c.send(f"HTTP/1.1 200 OK\n\n".encode() + open(f'.{c.recv(1024).decode().split()[1]}', 'r').read())
+  except FileNotFoundError: c.send("HTTP/1.1 404 Not Found".encode())
