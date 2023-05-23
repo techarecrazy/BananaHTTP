b71baffce8d2fa27fc08585cc9d4fb01efd67101
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-23 13:43:57 +0200

    Update generic.py

--- Server/generic.py
+++ Server/generic.py
@@ -7,4 +7,4 @@ s.listen(5)
 while True:
   c, _ = s.accept()
   try: c.send(b"HTTP/1.1 200 OK\n\n" + open(c.recv(1024).decode().split()[1], 'rb').read())
-  except FileNotFoundError, IsADirectoryError: c.send("HTTP/1.1 404 Not Found".encode("utf-8"))
+  except FileNotFoundError, IsADirectoryError: c.send(b"HTTP/1.1 404 Not Found")
