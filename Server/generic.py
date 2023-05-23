46f0d6bd174c58e62ddf73e1fcd8f4ba3cd9a716
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-23 13:35:39 +0200

    b string

--- Server/generic.py
+++ Server/generic.py
@@ -6,5 +6,5 @@ s.listen(5)
 
 while True:
   c, _ = s.accept()
-  try: c.send("HTTP/1.1 200 OK\n\n".encode() + open(c.recv(1024).decode().split()[1], 'rb').read())
+  try: c.send(b"HTTP/1.1 200 OK\n\n" + open(c.recv(1024).decode().split()[1], 'rb').read())
   except FileNotFoundError, IsADirectoryError: c.send("HTTP/1.1 404 Not Found".encode("utf-8"))
