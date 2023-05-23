54ddb7a8770df459e5ac1e1ad35210e9a786c3d3
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-23 18:09:41 +0200

    Update generic.py

--- Server/generic.py
+++ Server/generic.py
@@ -4,11 +4,7 @@ s = k.socket(k.AF_INET, k.SOCK_STREAM)
 s.bind(("0.0.0.0", 8080))
 s.listen(5)
 
-
 while True:
   c, _ = s.accept()
-  path = '.'+c.recv(1024).decode().split()[1]
   try: c.send(b"HTTP/1.1 200 OK\n\n" + open(path, 'rb').read())
-  except FileNotFoundError: c.send(b"HTTP/1.1 404 Not Found")
-  except IsADirectoryError: c.send(b"HTTP/1.1 200 OK\n\n" + open(path+'/index.html', 'r').read())
-  except: c.send(b"HTTP/1.1 500 Internal Server Error")
+  except (FileNotFoundError, IsADirectoryError): c.send(b"HTTP/1.1 404 Not Found")
