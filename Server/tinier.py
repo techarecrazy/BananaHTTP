2d644b83004b7be0bca9c599782aec8a3b025643
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-25 09:58:20 +0200

    Update tinier.py

--- Server/tinier.py
+++ Server/tinier.py
@@ -4,7 +4,7 @@ s = k.socket(k.AF_INET, k.SOCK_STREAM)
 s.bind(("0.0.0.0", 8080))
 s.listen(5)
 
-while True:
+while 1:
   c, _ = s.accept()
   try: c.send(b"HTTP/1.1 200 OK\n\n"+open('.'+c.recv(1024).decode().split()[1], 'rb').read())
   except: c.send(b"HTTP/1.1 404 Not Found")
