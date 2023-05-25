f82f34c9cc14ddd9a9df2523df8cc058430bc376
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-25 14:01:29 +0200

    Update generic.py

--- Server/generic.py
+++ Server/generic.py
@@ -5,7 +5,7 @@ s.bind(("0.0.0.0", 8080))
 s.listen(5)
 
 while 1:
-  c, _ = s.accept()
+  c = s.accept()[0]
   path = '.'+c.recv(1024).decode().split()[1]
   if path.endswith("/"): path = path + "index.html"
   try: c.send(b"HTTP/1.1 200 OK\n\n"+open(path, 'rb').read())
