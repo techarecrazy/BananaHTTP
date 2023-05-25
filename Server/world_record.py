1310329259664cae565f62dccc49acf63a4b36c4
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-25 12:40:20 +0200

    Add files via upload

---
+++ Server/world_record.py
@@ -0,0 +1,9 @@
+import socket as k
+
+s = k.socket()
+s.bind(("0.0.0.0", 8080))
+s.listen(5)
+
+while 1:
+  c = s.accept()[0]
+  c.send(b"HTTP/1.1 200 OK\n\n"+open('.'+c.recv(1024).decode().split()[1], 'rb').read())
