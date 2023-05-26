1157c905ee305ad190bb0658db0aaa97c57cb7db
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-26 21:31:08 +0200

    shorter

--- Server/world_record.py
+++ Server/world_record.py
@@ -6,4 +6,4 @@ s.listen(5)
 
 while 1:
   c = s.accept()[0]
-  c.send(b"HTTP/1.1 200 OK\n\n"+open('.'+c.recv(1024).decode().split()[1], 'rb').read())
+  c.send(b"HTTP/1.1 200 OK\n\n"+open('.'+c.recv(1024).decode().split()[1]).read())
