286c55a1bc7656ca500b25e198332447ecada8e7
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-26 21:41:43 +0200

    168 bytes

--- Server/world_record.py
+++ Server/world_record.py
@@ -4,4 +4,6 @@ s = k.socket()
 s.bind(("", 8080))
 s.listen(5)
 
-while 1: s.accept()[0].send(b"HTTP/1.1 200 OK\n\n"+open('index.html').read())
+while 1:
+  c = s.accept()[0]
+  c.send(b"HTTP/1.1 200 OK\n\n"+open(c.recv(1024).split[1],"rb").read())
