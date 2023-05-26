9383bad622e01862642be24f5f6bdf399164fc89
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-26 22:38:42 +0200

    Update world_record.py

--- Server/world_record.py
+++ Server/world_record.py
@@ -1,9 +1,9 @@
 import socket as k
 
-s = k.socket()
-s.bind(("", 8080))
+s=k.socket()
+s.bind(("",8080))
 s.listen(5)
 
 while 1:
-  c = s.accept()[0]
+  c=s.accept()[0]
   c.send(b"HTTP/1.1 200 OK\n\n"+open(c.recv(1024).decode().split[1],"rb").read())
