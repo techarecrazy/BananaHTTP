c2146cb6d5473b74bf53fe609f6bb40cc8de04c3
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-27 14:09:21 +0200

    secure.py

--- Server/secure.py
+++ Server/secure.py
@@ -1,21 +1,15 @@
 import socket as k
 import re
+
 s = k.socket()
-
 s.bind(("", 8080))
-
 s.listen(5)
 
 while 1:
-
   c=s.accept()[0]
-  
   path='.'+c.recv(1024).decode().split()[1]
   if ".." in re.split("/",path): c.send(b"HTTP/1.1 200 OK\n\n Please don't access our secret files - The admin of the server"); c.close()
   if path.endswith("/"): path+="index.html"
-
   try: c.send(b"HTTP/1.1 200 OK\n\n"+open(path, 'rb').read())
-
   except: c.send(b"HTTP/1.1 404 Not Found")
-
   c.close()
