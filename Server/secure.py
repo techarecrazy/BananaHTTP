6af2ace5cad7e3b993208b2be158dae45daff6da
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-27 14:34:49 +0200

    Update secure.py

--- Server/secure.py
+++ Server/secure.py
@@ -8,7 +8,7 @@ s.listen(5)
 while 1:
   c=s.accept()[0]
   path='.'+c.recv(1024).decode().split()[1]
-  if ".." in re.split("/",path): c.send(b"HTTP/1.1 200 OK\n\n Please don't access our secret files - The admin of the server"); c.close()
+  if ".." in re.split("/",path): c.send(b"HTTP/1.1 403 Forbidden"); c.close()
   if path.endswith("/"): path+="index.html"
   try: c.send(b"HTTP/1.1 200 OK\n\n"+open(path, 'rb').read())
   except: c.send(b"HTTP/1.1 404 Not Found")
