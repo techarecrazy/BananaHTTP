7b85abfa09100eda15fe61234cd3d1a830167ea4
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-27 13:26:09 +0200

    Update world_record.py

--- Server/world_record.py
+++ Server/world_record.py
@@ -6,5 +6,5 @@ s.listen(5)
 
 while 1:
   c=s.accept()[0]
-  try: c.send(b"HTTP/1.1 200 OK\n\n"+open(c.recv(1024).decode().split[1][1:]).read())
-  except: c.send(b"HTTP/1.1 404 Not Found")
+  c.send(b"HTTP/1.1 200 OK\n\n"+open(c.recv(1024).decode().split[1][1:]).read())
+  
