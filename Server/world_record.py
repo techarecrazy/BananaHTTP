33f68ce4a564219c378d2ab831a3f07f707ad3d4
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-27 13:34:43 +0200

    Update world_record.py

--- Server/world_record.py
+++ Server/world_record.py
@@ -6,5 +6,4 @@ s.listen(5)
 
 while 1:
   c=s.accept()[0]
-  c.send(b"HTTP/1.1 200 OK\n\n"+open(c.recv(1024).decode().split[1][1:]).read())
-  
+  c.send(b"HTTP/1.1 200 OK\n\n"+open(c.recv(1024).decode().split()[1][1:]).read())
