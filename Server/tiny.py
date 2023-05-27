b1058dad630983871e0a09464572f521b1011e19
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-27 13:29:03 +0200

    Update tiny.py

--- Server/tiny.py
+++ Server/tiny.py
@@ -1,10 +1,10 @@
 import socket as k
 
 s=k.socket()
-s.bind(("", 8080))
+s.bind(("",8080))
 s.listen(5)
 
 while 1:
   c=s.accept()[0]
-  try: c.send(b"HTTP/1.1 200 OK\n\n"+open('.'+c.recv(1024).decode().split()[1], 'rb').read())
+  try: c.send(b"HTTP/1.1 200 OK\n\n"+open(c.recv(1024).decode().split()[1][1:],'rb').read())
   except: c.send(b"HTTP/1.1 404 Not Found")
