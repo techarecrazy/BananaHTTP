894661f241c9f9bf0ff04a8ec231847103c6953f
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-27 13:37:18 +0200

    Update world_record.py

--- Server/world_record.py
+++ Server/world_record.py
@@ -4,5 +4,4 @@ s=k.socket()
 s.bind(("",8080))
 s.listen(5)
 
-while 1:
-  s.accept()[0].send(b"HTTP/1.1 200 OK\n\n"+open("index.html").read())
+while 1: s.accept()[0].send(b"HTTP/1.1 200 OK\n\n"+open("index.html").read())
