a9722bca7062d21fa86c6345f565b7a40c6fa494
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-27 13:44:52 +0200

    Update generic.py

--- Server/generic.py
+++ Server/generic.py
@@ -5,8 +5,9 @@ s.bind(("", 8080))
 s.listen(5)
 
 while 1:
-  c = s.accept()[0]
-  path = '.'+c.recv(1024).decode().split()[1]
-  if path.endswith("/"): path += "index.html"
+  c=s.accept()[0]
+  path='.'+c.recv(1024).decode().split()[1]
+  if path.endswith("/"): path+="index.html"
   try: c.send(b"HTTP/1.1 200 OK\n\n"+open(path, 'rb').read())
   except: c.send(b"HTTP/1.1 404 Not Found")
+  c.close()
