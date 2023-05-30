e9e3f4baed63b816da77c3c73cd3b474d5ecb2de
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-30 22:19:43 +0200

    Update main.py

--- Server/full/main.py
+++ Server/full/main.py
@@ -11,7 +11,7 @@ while 1:
   p='.'+c.recv(1024).decode().split()[1]
   mi = m.guess_type(path))[0]
   if ".." in path: c.send("HTTP/1.1 403 Forbidden")
-  if re.split('/', mi in ("video", "audio"): c.send(open(mi+".html", 'rb').read().format(p=p))
+  if re.split('/', mi) in ("video", "audio"): c.send(open(mi+".html", 'rb').read().format(p=p))
   if p.endswith("/"): p+="index.html"
   try: c.send(b"HTTP/1.1 200 OK\n\n"+open(p, "rb").read())
   except: c.send(b"HTTP/1.1 404 Not Found")
