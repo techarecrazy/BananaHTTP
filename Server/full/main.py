4ae859059b5dbc801d438c0c1220804f9e7290a5
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-30 20:51:52 +0200

    Update main.py

--- Server/full/main.py
+++ Server/full/main.py
@@ -1,8 +1,8 @@
-import socket as k
+import socket
 import mimetypes as m
 import re
 
-s = k.socket()
+s = socket.socket()
 s.bind(("", 8080))
 s.listen(5)
 
@@ -12,7 +12,7 @@ while 1:
   mime=re.split('/', m.guess_type(path))[0]
   def r(): c.send(open(mime+".html", 'rb').read().format(path=path))
   if ".." in path: c.send("HTTP/1.1 403 Forbidden")
-  if "audio" or "video" == mime: r()
+  if mime in ["video", "audio"]: r()
   if path.endswith("/"): path+="index.html"
   try: c.send(b"HTTP/1.1 200 OK\n\n"+open(path, "rb").read())
   except: c.send(b"HTTP/1.1 404 Not Found")
