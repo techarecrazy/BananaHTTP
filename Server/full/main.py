f2947b20adb838a87a1f8762b1e4243f76dacf4c
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-05-30 20:53:07 +0200

    Update main.py

--- Server/full/main.py
+++ Server/full/main.py
@@ -10,9 +10,8 @@ while 1:
   c=s.accept()[0]
   path='.'+c.recv(1024).decode().split()[1]
   mime=re.split('/', m.guess_type(path))[0]
-  def r(): c.send(open(mime+".html", 'rb').read().format(path=path))
   if ".." in path: c.send("HTTP/1.1 403 Forbidden")
-  if mime in ["video", "audio"]: r()
+  if mime in ["video", "audio"]: c.send(open(mime+".html", 'rb').read().format(path=path))
   if path.endswith("/"): path+="index.html"
   try: c.send(b"HTTP/1.1 200 OK\n\n"+open(path, "rb").read())
   except: c.send(b"HTTP/1.1 404 Not Found")
