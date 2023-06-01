8f2f03657a623468a2776c7e8c2d7d7f5a631a0a
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-06-01 15:11:15 +0200

    Update main.py

--- Server/record/main.py
+++ Server/record/main.py
@@ -2,6 +2,4 @@ from pyshort import tcp
 s=tcp(8080)
 while 1:
   c=s.accept()[0]
-  try: c.send(b"HTTP/1.1 200 OK\n\n"+open(c.recv(1024).decode().split()[1][1:],'rb').read())
-  except: c.send(b"HTTP/1.1 404 Not Found")
-  c.close()
+  c.send(b"HTTP/1.1 200 OK\n\n"+open(c.recv(1024).decode().split()[1][1:],'rb').read())
