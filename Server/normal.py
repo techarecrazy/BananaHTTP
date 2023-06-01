78b97ce77840564a355fce6a5fd939ce84a254fb
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-06-01 15:44:18 +0200

    Update normal.py

--- Server/normal.py
+++ Server/normal.py
@@ -8,6 +8,7 @@ while 1:
   c=s.accept()[0]
   path='.'+c.recv(1024).decode().split()[1]
   if path.endswith("/"): path+="index.html"
+  if ".." in path: c.send("HTTP/1.1 403 Forbidden")
   try: c.send(b"HTTP/1.1 200 OK\n\n"+open(path, 'rb').read())
   except: c.send(b"HTTP/1.1 404 Not Found")
   c.close()
