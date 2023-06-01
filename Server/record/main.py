ea53f9360dc350e2e3f1983099357e879948b65e
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-06-01 15:29:33 +0200

    Delete Server/record directory

--- Server/record/main.py
+++
@@ -1,2 +0,0 @@
-from pyshort import tcp
-tcp(8080).send(b"HTTP/1.1 200 OK\n\nHello")
--- Server/record/pyshort.py
+++
@@ -1,6 +0,0 @@
-import socket
-def tcp(port):
-  s = socket.socket()
-  s.bind(("", port))
-  s.listen(5)
-  return s.accept()[0]
