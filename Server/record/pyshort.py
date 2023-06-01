168ce038429f95abf49ad1ea284c39cf01647962
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-06-01 15:16:49 +0200

    Update pyshort.py

--- Server/record/pyshort.py
+++ Server/record/pyshort.py
@@ -1,6 +1,6 @@
 import socket
-  def tcp_srv(self, port):
-    s = socket.socket()
-    s.bind(("", port))
-    s.listen(5)
-    return s.accept()[0]
+def tcp(port):
+  s = socket.socket()
+  s.bind(("", port))
+  s.listen(5)
+  return s.accept()[0]
