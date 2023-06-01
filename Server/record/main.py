1555a480e0b900f6e54656add180077801427d6e
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-06-01 15:11:55 +0200

    Update main.py

--- Server/record/main.py
+++ Server/record/main.py
@@ -2,4 +2,4 @@ from pyshort import tcp
 s=tcp(8080)
 while 1:
   c=s.accept()[0]
-  c.send(b"HTTP/1.1 200 OK\n\n"+open(c.recv(1024).decode().split()[1][1:],'rb').read())
+  c.send(b"HTTP/1.1 200 OK\n\n"+open("index.html",'rb').read())
