735b4853579d84bf16459ef016ad7aea39b277e4
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-06-03 20:49:24 +0200

    Update full.py

--- Server/full.py
+++ Server/full.py
@@ -15,8 +15,9 @@ while 1:
     for f in os.listdir(path): t+=f"<a href='{f}'>{f}</a><br>"
     c.send(b"HTTP/1.1 200 OK\n\n<html><body><h1>Directory listing:</h1><br>"+t.encode()+"</body></html>")
   else:
-    try: with open(path, 'rb') as f:
-      c.send(b"HTTP/1.1 200 OK\n\n"+f.read())
-      f.close()
+    try: 
+      with open(path, 'rb') as f:
+        c.send(b"HTTP/1.1 200 OK\n\n"+f.read())
+        f.close()
     except: c.send(b"HTTP/1.1 404 Not Found")
   c.close()
