de245efcaa6aa3426c9e52d15dcaa7e7043c2195
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-06-03 19:33:51 +0200

    Update full.py

--- Server/full.py
+++ Server/full.py
@@ -10,8 +10,8 @@ while 1:
   path='.'+c.recv(1024).decode().split()[1]
   elif ".." or "//" in path: c.send(b"HTTP/1.1 403 Forbidden")
   if path.endswith("/"): 
-    t="<html><body><h1>Directory listing:</h1><br>"
-    for f in os.listdir(): if f != "index.html": t += f"<a href='{f}'>{f}</a><br>"; else: path += "index.html"
+    t=f"<html><body><h1>Directory listing of {path[1:]}:</h1><br>"
+    for f in os.listdir(path): if f != "index.html": t += f"<a href='{f}'>{f}</a><br>"; else: path += "index.html"
     t+="</body></html>"
     c.send(b"HTTP/1.1 200 OK\n\n"+t.encode())
   else: send(c)
