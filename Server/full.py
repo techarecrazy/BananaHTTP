a85f0184b000f6555d61c5211a1be243a38a4bae
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-06-05 18:30:22 +0200

    Update full.py

--- Server/full.py
+++ Server/full.py
@@ -9,7 +9,8 @@ s.listen(5)
 while 1:
   c=s.accept()[0]
   try: path='.'+c.recv(1024).decode().split()[1]
-  except IndexError: continue;
+  except IndexError: break;
+  if os.path.isdir(p) and not p[-1]=="/": p+="/"
   if ".." or "//" in path: c.send(b"HTTP/1.1 403 Forbidden")
   if path[-1]=="/":
     t=""
