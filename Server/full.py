1a85aef256427e3a43da91b37b5482fdc3c4fe47
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-06-05 09:02:29 +0200

    Update full.py

--- Server/full.py
+++ Server/full.py
@@ -5,15 +5,17 @@ s = k.socket()
 s.bind(("", 8080))
 s.listen(5)
 
+def dir(p): 
+  if os.path.isdir(p) and not p[-1]=="/": return p+"/"
+  else: return p
 while 1:
   c=s.accept()[0]
   try: path='.'+c.recv(1024).decode().split()[1]
   except IndexError: continue;
-  if os.path.isdir(path) and not path.endswith("/"): path += "/"
   if ".." or "//" in path: c.send(b"HTTP/1.1 403 Forbidden")
   if path[-1]=="/":
     t=""
-    for f in os.listdir(path): t+=f"<a href='{f}'>{f}</a><br>"
+    for f in os.listdir(path): t+=f"<a href='{dir(f)}'>{f}</a><br>"
     c.send(b"HTTP/1.1 200 OK\n\n<html><body><h1>Directory listing:</h1><br>"+t.encode()+b"</body></html>")
   else:
     try: 
