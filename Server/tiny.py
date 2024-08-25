fac6ea2c0643c330798d50b95c65692c116e0112
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2024-08-25 22:57:14 +0200

    Update tiny.py

--- Server/tiny.py
+++ Server/tiny.py
@@ -4,7 +4,7 @@ s.listen(1)
 
 while 1:
   c = s.accept()[0]
-  try: r= b"HTTP/1.1 200 OK\n\n" + open(str(c.recv(1024)).split()[1][1:],'rb').read()
+  try: r= b"HTTP/1.1 200 OK\n\n" + open(str(c.recv()).split()[1][1:],'rb').read()
   except: r= b"HTTP/1.1 404 Not Found"
   c.send(r)
   c.close()
